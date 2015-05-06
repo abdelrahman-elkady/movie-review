f8e4b880491193029f42c9af1f9db1269371cf3c
kady <elkadyabdelrahman@gmail.com>
2015-05-06 14:45:21 +0200

    Using bower , installed bootstrap

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# Using bower
+config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+
 module MovieReview
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
