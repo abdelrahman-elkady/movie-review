717a39959e45b1627b060832cd641c56b67e7135
kady <elkadyabdelrahman@gmail.com>
2015-05-06 12:01:58 +0200

    Nesting resources for movies and reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,8 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :set_movie
   before_action user.authenticate!
-  
-  # GET /reviews
-  # GET /reviews.json
-  def index
-    @reviews = Review.all
-  end
 
-  # GET /reviews/1
-  # GET /reviews/1.json
-  def show
-  end
 
   # GET /reviews/new
   def new
@@ -26,6 +17,8 @@ class ReviewsController < ApplicationController
   # POST /reviews.json
   def create
     @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    @review.movie_id = @movie.id
 
     respond_to do |format|
       if @review.save
@@ -66,6 +59,10 @@ class ReviewsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_review
       @review = Review.find(params[:id])
+    end
+
+    def set_movie
+      @movie = Movie.find_by_id(params[:movie_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
