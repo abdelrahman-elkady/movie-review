c66ff09f2b4da0339b43b4168d1277419d3797d6
kady <elkadyabdelrahman@gmail.com>
2015-05-11 00:38:37 +0200

    Adding average rating to movies model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,4 +4,27 @@ class Movie < ActiveRecord::Base
 
   has_attached_file :image, :styles => { :medium => "400x600#" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+  def avg_rate
+    reviews = Review.where(movie_id: self.id)
+
+
+    avg = 0
+    count = 0
+
+    reviews.each do |review|
+      if review.rate.present?
+        count += 1
+        avg += review.rate
+      end
+    end
+
+    if count != 0
+      avg = avg / count*1.0
+    else
+      avg = 0.0
+    end
+
+
+  end
 end
