edc628746a8000d06872b3f9f9c4d1c9ff89fe99
kady <elkadyabdelrahman@gmail.com>
2015-05-13 22:09:04 +0200

    Adding Reviews section for movie show

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,17 +4,13 @@ class ReviewsController < ApplicationController
   before_action :authenticate_user!
 
 
-  # GET /reviews/new
   def new
     @review = Review.new
   end
 
-  # GET /reviews/1/edit
   def edit
   end
 
-  # POST /reviews
-  # POST /reviews.json
   def create
     @review = Review.new(review_params)
     @review.user_id = current_user.id
@@ -31,8 +27,6 @@ class ReviewsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /reviews/1
-  # PATCH/PUT /reviews/1.json
   def update
     respond_to do |format|
       if @review.update(review_params)
@@ -45,8 +39,6 @@ class ReviewsController < ApplicationController
     end
   end
 
-  # DELETE /reviews/1
-  # DELETE /reviews/1.json
   def destroy
     @review.destroy
     respond_to do |format|
